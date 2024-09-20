48cd01e57a8c24367ac533d65745eed451774e58
Rachit Srivastava <honey032004@gmail.com>
2024-09-21 00:31:22 +0530

    C++CompilerKeymap

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -17,5 +17,19 @@ vim.opt.shiftwidth = 2
 vim.opt.shiftround = true
 vim.opt.expandtab = true
 
-vim.keymap.set('n','<leader>h', ':nohlsearch<CR>')
 
+--C++ compilation keymap
+function compile_and_run_c()
+  local filename = vim.fn.expand('%:p')
+  local output = vim.fn.fnamemodify(filename, ':r') .. '.exe'
+
+  local compile_cmd = string.format('g++ -std=c++17 "%s" -o "%s"', filename, output)
+  local run_cmd = string.format('"%s"', output)
+
+  vim.cmd("vsplit | term")
+  
+  vim.fn.chansend(vim.b.terminal_job_id, compile_cmd .. "\n")
+  vim.fn.chansend(vim.b.terminal_job_id, run_cmd .. "\n")
+end
+
+vim.api.nvim_set_keymap('n', '<C-r>', ':lua compile_and_run_c()<CR>', { noremap = true, silent = true })
